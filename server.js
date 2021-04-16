48fc91ac5142fb9ac19a0cd1ee538117f1369093
Ironfacebuster <willeston202@gmail.com>
2021-04-16 18:05:59 -0400

    improved server.js

--- server.js
+++ server.js
@@ -5,24 +5,34 @@ const public = "./public"
 var port = process.env.PORT || 4040
 
 const server = http.createServer((req, res) => {
-    const url = `./public/${req.url}`
-    fs.access(url, fs.constants.R_OK, (err) => {
-        if (err) {
-            console.error(err)
-            res.writeHead(403, "Forbidden.")
-            return res.end(`<h1>403 - Forbidden.</h1> You are forbidden to access <b>${req.url}</b>.`)
-        } else {
-            if (fs.existsSync(url)) {
-                res.write(fs.readFileSync(url) || null)
-                return res.end()
-            } else {
-                res.writeHead(404, "Not found.")
-                return res.end(`<h1>404 - Not Found.</h1> <b>${req.url}</b> was not found.`)
-            }
-        }
-    })
+    const url = `./public${req.url}`
+    console.log(url)
+    if (url == "./public/") {
+        res.write("<h1>Index</h1>hello :)")
+        return res.end()
+    }
+
+    if (fs.existsSync(url)) {
+        res.write(fs.readFileSync(url) || null)
+        return res.end()
+    } else {
+        res.writeHead(404, "Not found.")
+        return res.end(`<h1>404 - Not Found.</h1> <b>${req.url}</b> was not found.`)
+    }
 })
 
 server.listen(port, () => {
     console.log("listening on port", port)
 })
+
+function ScanDirectory(dir) {
+    var obj = {}
+    fs.readdirSync(dir).forEach(d => {
+        if (fs.lstatSync(dir + "/" + d).isDirectory()) {
+            obj[d] = ScanDirectory(dir + "/" + d)
+        } else obj[d] = fs.readFileSync(dir + "/" + d)
+    })
+    return obj
+}
+
+var directories = ScanDirectory(public)
