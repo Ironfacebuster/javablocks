c537fd586af94baeba36aa38731a42f0008e7f01
Ironfacebuster <willeston202@gmail.com>
2021-04-16 17:54:38 -0400

    Access check before reading

--- server.js
+++ server.js
@@ -5,14 +5,22 @@ const public = "./public"
 var port = process.env.PORT || 4040
 
 const server = http.createServer((req, res) => {
-    if (fs.existsSync(`./public/${req.url}`)) {
-        res.write(fs.readFileSync(`./public/${req.url}`) || null)
-        return res.end()
-    } else {
-        res.writeHead(404, "Not found.")
-        return res.end(`<h1>404 - Not Found.</h1> <b>${req.url}</b> was not found.`)
-    }
-
+    const url = `./public/${req.url}`
+    fs.access(url, fs.constants.R_OK, (err) => {
+        if (err) {
+            console.error(err)
+            res.writeHead(403, "Forbidden.")
+            return res.end(`<h1>403 - Forbidden.</h1> You are forbidden to access <b>${req.url}</b>.`)
+        } else {
+            if (fs.existsSync(url)) {
+                res.write(fs.readFileSync(url) || null)
+                return res.end()
+            } else {
+                res.writeHead(404, "Not found.")
+                return res.end(`<h1>404 - Not Found.</h1> <b>${req.url}</b> was not found.`)
+            }
+        }
+    })
 })
 
 server.listen(port, () => {
