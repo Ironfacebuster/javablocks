d85c096d2c4f74773087b8b0551f9746b736d310
Ironfacebuster <willeston202@gmail.com>
2021-04-16 17:43:28 -0400

    Changes for heroku

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const fs = require("fs")
 const http = require("http")
 
+const public = "./public"
+var port = process.env.$PORT || 4040
+
 const server = http.createServer((req, res) => {
-    if (fs.existsSync(`.${req.url}`))
-        res.write(fs.readFileSync(`.${req.url}`) || null)
-    res.end()
+    if (fs.existsSync(`./public/${req.url}`)) {
+        res.write(fs.readFileSync(`./public/${req.url}`) || null)
+        return res.end()
+    } else {
+        res.writeHead(404, "Not found.")
+        return res.end(`<h1>404 - Not Found.</h1> <b>${req.url}</b> was not found.`)
+    }
+
 })
 
-server.listen(4040, () => {
-    console.log("listening on port 4040")
+server.listen(port, () => {
+    console.log("listening on port", port)
 })
